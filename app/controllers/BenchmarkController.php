754119c3bf31922d49bf1ccba96d655860f81038
Yngve Høiseth <yngve@hoiseth.net>
2018-02-15 17:08:27 +0100

    Hello World

---
+++ app/controllers/BenchmarkController.php
@@ -0,0 +1,17 @@
+<?php
+
+class BenchmarkController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function helloWorldAction()
+    {
+        echo 'Hello World!';
+    }
+
+}
+
